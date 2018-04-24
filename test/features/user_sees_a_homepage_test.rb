5fd57446779c88fe8463e8fce1a6fc63d45e7c93
Cole Hart <cole@colehart.com>
2018-04-24 16:39:16 -0600

    added about.html and test

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,18 @@ class HompageTest < CapybaraTestCase
     assert page.has_content?('Welcome!')
     assert_equal 200, page.status_code
   end
+
+  def test_user_can_see_error_message
+    visit '/error'
+
+    assert page.has_content?('Page not found.')
+    assert_equal 404, page.status_code
+  end
+
+  def test_user_can_see_about_page
+    visit '/about'
+
+    assert page.has_content?("About Me!\nHere's some stuff to know.")
+    assert_equal 200, page.status_code
+  end
 end
